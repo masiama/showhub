f617b1c513f4b8790bac3d9e32323e16c56ab8a5
Leonid Cupikov <leonid.cupikov@gmail.com>
2026-04-30 06:48:08 +0200

    feat: add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">Hello World!</h1>
+  <RouterView />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import vueRouter from "vue-router/vite";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vueRouter(), vue(), tailwindcss()],
 });
